51899b4d4d73c6f2763244fc3e43c53ed3797d8f
Mackenzie <clarkmackenzieb@gmail.com>
2017-10-09 15:54:40 -0500

    work on project

--- server/index.js
+++ server/index.js
@@ -7,14 +7,19 @@ const cors = require('cors');
 const passport = require('passport');
 const Auth0Strategy = require('passport-auth0');
 
+const dogAPICtrl = require('./dogAPICtrl');
+
 const config = require('./config');
-const { secret } = require('./config').session;
-const { dbUser, database } = require('./config').db;
-const { domain, clientID, clientSecret } = require('./config').auth0;
+const { secret } = require('./config');
+// .session ? 
+const { dbUser, dbPass, database } = require('./config');
+// .db ?
+const { domain, clientID, clientSecret } = require('./config');
+//.auth0 ?
 
 const port = 3005;
 
-const connectionString = `postgress://${dbUser}@localhost/${database}`;
+const connectionString = `postgress://${dbUser}:${dbPass}@localhost/${database}`;
 
 const app = express();
 
@@ -25,3 +30,86 @@ app.use(express.static(`${__dirname}/../public`));
 massive(connectionString).then(db => app.set('db', db));
 
 // put passport stuff here cause that shit is WILD
+// setting up express sessions
+// secret: config.session.secret;
+app.use(session({
+    secret,
+    resave: true,
+    saveUninitialized: true
+}));
+
+// setting up passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// using passport to access auth0
+// { domain: config.auth0.domain ... etc}
+passport.use(new Auth0Strategy({
+    domain,
+    clientID,
+    clientSecret,
+    callbackURL:  '/login/callback'
+   }, (accessToken, refreshToken, extraParams, profile, done) => {
+     //Find user in database
+     console.log(profile.id);
+     const db = app.get('db');
+     // .then means this is a promise
+     db.getUserByAuthId([profile.id]).then((user, err) => {
+         console.log('INITIAL: ', user);
+       if (!user[0]) { //if there isn't a user, we'll create one!
+         console.log('CREATING USER');
+         db.createUserByAuth([profile.displayName, profile.id]).then((user, err) => {
+           console.log('USER CREATED', user[0]);
+           return done(err, user[0]); // GOES TO SERIALIZE USER
+         })
+       } else { //when we find the user, return it
+         console.log('FOUND USER', user[0]);
+         return done(err, user[0]);
+       }
+     });
+   }
+ ));
+
+ // put user on session
+ passport.serializeUser((user, done) => {
+     done(null, user);
+ });
+
+ // pull user from session for manipulation
+ passport.deserializeUser((user, done) => {
+     console.log(user);
+     done(null, user);
+ });
+
+
+// auth endpoints
+
+// initial endpoint to fire off login
+app.get('/login', passport.authenticate('auth0'));
+
+// redirect to home and use the resolve to catch the user where we redirect them to
+app.get('/login/callback',
+    passport.authenticate('auth0', { successRedirect: '/' }), (req, res) => {
+        res.status(200).json(req.user);
+});
+
+// if not logged in, send error message and catch in resolve
+// else send user
+app.get('/user', (req, res) => {
+    if (!req.user) return res.status(401).json({err: 'User Not Authenticated'});
+    res.status(200).json(req.user);
+});
+
+// remove user from session
+app.get('/user/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
+
+
+//API Endpoints
+// app.get('https://dog.ceo/api/breeds/image/random', dogAPICtrl.randomdog);
+
+app.listen(port, ()=>{
+    console.log(`I'll be right by your side till ${port}`)
+});
